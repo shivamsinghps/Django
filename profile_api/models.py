5dc2cee6ba760f60e3a1421800b77ec73601a7a0
Shivam Singh <shivamsinghps@gamil.com>
2021-11-06 11:18:11 -0700

    custom user moel created

--- profile_api/models.py
+++ profile_api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin 
+from django.contrib.auth.models import BaseUserManager 
 
-# Create your models here.
+class UserProfileManager(BaseUserManager):
+
+    def create_user(email,name,password=None):
+        if not email:
+            raise ValueError("User must have an email")
+        email = self.normalize_email(email)
+        user = self.model(email=email,name=name)
+        user.set_password(password)
+        user.save(using=self.db)
+
+        return user
+    
+    def create_super_user(email,name,password):
+        if not email:
+            raise ValueError("User must have an email")
+        user = self.create_user(email,name,password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self.db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name        
+
+    def __str__(self):
+        return self.email
+    
